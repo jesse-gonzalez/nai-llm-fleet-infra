bdee23313ae49c3ec403df3581e92d22183b0fb5
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-03-21 15:09:31 +0000

    fix: devcontainer

--- .devcontainer/ci/features/install.sh
+++ .devcontainer/ci/features/install.sh
@@ -5,7 +5,7 @@ set -o noglob
 apk add --no-cache \
     bash bind-tools ca-certificates curl python3 \
         py3-pip moreutils jq git iputils \
-            openssh-client fzf
+            openssh-client starship fzf
 
 apk add --no-cache \
     --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community \
@@ -32,23 +32,30 @@ done
 mkdir -p /home/vscode/.config/bash/{completions,conf.d}
 
 # Setup autocompletions for bash
-for tool in cilium flux helm k9s kubectl kustomize talhelper talosctl; do
+for tool in flux helm k9s kubectl kustomize; do
     $tool completion bash > /home/vscode/.config/bash/completions/$tool.bash
 done
 gh completion --shell bash > /home/vscode/.config/bash/completions/gh.bash
 stern --completion bash > /home/vscode/.config/bash/completions/stern.bash
 yq shell-completion bash > /home/vscode/.config/bash/completions/yq.bash
 
-# # Add aliases into bash
-# tee /home/vscode/.config/bash/conf.d/aliases.bash > /dev/null <<EOF
-# alias ls lsd
-# alias kubectl kubecolor
-# alias k kubectl
-# EOF
+# Add hooks into bash
+tee /home/vscode/.config/bash/conf.d/hooks.bash > /dev/null <<EOF
+if status is-interactive
+    starship init bash | source
+end
+EOF
+
+# Add aliases into bash
+tee /home/vscode/.config/bash/conf.d/aliases.bash > /dev/null <<EOF
+alias ls lsd
+alias kubectl kubecolor
+alias k kubectl
+EOF
 
 # # Custom bash prompt
-# tee /home/vscode/.config/bash/conf.d/fish_greeting.fish > /dev/null <<EOF
-# function fish_greeting
+# tee /home/vscode/.config/bash/conf.d/bash_greeting.bash > /dev/null <<EOF
+# function bash_greeting
 #     echo (set_color yellow)"Welcome! Press [enter] if you see a direnv error"
 # end
 # EOF
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,7 @@
 set -e
 set -o noglob
 
-## install task
-#[ -n "$(command -v arkade)" ] || sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
+## install task if not available
+[ -n "$(command -v task)" ] || sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
 
 task workstation:arkade
