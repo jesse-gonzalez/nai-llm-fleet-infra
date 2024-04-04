7581f923274f15b3a58a686765ac802d0ecd9a6c
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-04-04 16:49:08 -0400

    fix: add devbox for testing

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -o noglob
-
-## install task if not available
-#[ -n "$(command -v task)" ] || sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
-
-#task workstation:arkade
