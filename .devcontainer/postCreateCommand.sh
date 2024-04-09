a28edaab3522f50ab58c245fe34cd69a9faf8d7c
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-04-09 09:41:13 -0400

    add post create command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,8 @@
 set -e
 set -o noglob
 
-## install task if not available
+## configure local dot files
 [ -n "$(command -v chezmoi)" ] || chezmoi init --apply https://github.com/$GITHUB_USERNAME/dotfiles.git
 
+## install krew packages
 task workstation:krew
