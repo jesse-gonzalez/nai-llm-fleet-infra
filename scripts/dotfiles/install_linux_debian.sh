d11d8e6ba93f23511f1a57b6af832d469745a23f
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-04-12 09:38:25 -0400

    add: dotfiles local

--- scripts/dotfiles/install_linux_debian.sh
+++ scripts/dotfiles/install_linux_debian.sh
@@ -5,7 +5,11 @@ set -o pipefail
 
 ## install common debian packages
 if [  -n "$(cat /etc/lsb-release | grep -i ubuntu)" ]; then
-    sudo apt-get update -y
-    sudo apt-get install -y \
-      wget
+  sudo apt-get update -y && \
+  sudo apt-get install -y \
+    wget \
+    tree \
+    vim \
+    bash-completion \
+    python3
 fi
--- scripts/dotfiles/install_zsh.sh
+++ scripts/dotfiles/install_zsh.sh
@@ -3,6 +3,13 @@
 set -ex
 set -o pipefail
 
+## install common debian packages
+if [  -n "$(cat /etc/lsb-release | grep -i ubuntu)" ]; then
+  sudo apt-get update -y && \
+  sudo apt-get install -y \
+    zsh
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --dissociate https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --dissociate https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
