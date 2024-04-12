31ffcc333e76fc3789bd560e55e83fe69d938113
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-04-12 12:02:40 -0400

    fix: attempt to add kube linter

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -39,11 +39,23 @@ echo "INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
-find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating $file"
-    yq e 'true' "$file" > /dev/null
-done
+# echo "INFO - Validating apps"
+# find ./apps -maxdepth 4 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+#   do
+#     kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
+#     if [[ ${PIPESTATUS[0]} != 0 ]]; then
+#       exit 1
+#     fi
+# done
+
+# echo "INFO - Validating platform services"
+# find ./platforms -maxdepth 4 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+#   do
+#     kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
+#     if [[ ${PIPESTATUS[0]} != 0 ]]; then
+#       exit 1
+#     fi
+# done
 
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
