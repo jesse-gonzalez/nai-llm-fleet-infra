fcdecbde3ddbdb0c102491e64cc3d3e265060f01
jesse-gonzalez <jesse.gonzalez@nutanix.com>
2024-04-21 23:51:35 -0400

    fix: leverage debian as os-release check vs. ubuntu

--- scripts/common/install_linux_ipcalc.sh
+++ scripts/common/install_linux_ipcalc.sh
@@ -4,7 +4,7 @@ set -ex
 set -o pipefail
 
 ## install ipcalc package - https://snapcraft.io/install/gum/debian
-if grep -q -i ubuntu /etc/os-release; then
+if grep -q -i debian /etc/os-release; then
     sudo apt-get update -y
     sudo apt-get install -y ipcalc
 fi
